7f79ec0390be17df24b54d93728c6b625ba2aa21
Ann-Julie Sinnaeve <annjulie.sinnaeve@yahoo.de>
2021-02-11 19:20:56 +0100

    telegram bot local host implemented first real api request

--- VerteilteSystemeProjekt/src/main/bot/telegram-bot.py
+++ VerteilteSystemeProjekt/src/main/bot/telegram-bot.py
@@ -14,12 +14,40 @@ vSysChatId = "-507253319"
 # Nachrichten versenden
 # neue URL benötigt
 sendURL = "http://api.telegram.org/bot" + vSysBotToken + "/sendMessage"
+# url für lokal host für die api später
+portNumber = 4567
+
+getURL = "http://localhost:" + str(portNumber)
 
 def send_message(chatId, message):
     # die Ergebnisse aus den Variablen unten senden
     requests.post(sendURL + "?chat_id=" + str(chatId) + "&text=" + message)
 
-send_message(vSysChatId, "Welcome to this channel")
+"""
+wir wollen mit dem bot diese rest api wrappen
+dafür schreiben wir für jeden Zugriffspunkt eine Methode, die den jeweiligen Wert ausgibt
+"""
+def get_average_increase_last_n_days(days):
+    # über commands soll eine Abfrage erscheinen, nach Tagen
+    #
+    endPoint = "/averageincreaselastndays/"
+    return requests.get(getURL + endPoint + str(days))
+
+
+#send_message(vSysChatId, "Moin Meister")
+# so bekommen wir nur das request Objekt und müssen jetzt noch die
+# Daten daraus herausholen
+r = get_average_increase_last_n_days(5)
+print(r)
+print(r.text)
+
+""" 
+ab dem Zeitpunkt an dem es einen Server gibt, muss einfach nur die URL in getURL angepasst werden, feddich
+in den Chat mit send_Message einfügen, also nur überlegen wie Text von  A nach B kommt, jedesMal wenn Zugriffspunkt 
+aufgerufen wird
+todo morgen: Commands anschauen für Interaktion mit User in App
+"""
+
 
 
 

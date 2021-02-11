83f477825e6aae12b944fa1f8dfccbe89fc9840a
rohrii <78815405+rohrii@users.noreply.github.com>
2021-02-11 16:20:18 +0100

    Author

--- VerteilteSystemeProjekt/src/main/java/de/vsys/project/general/data/ForecastNecessaryLockdownDays.java
+++ VerteilteSystemeProjekt/src/main/java/de/vsys/project/general/data/ForecastNecessaryLockdownDays.java
@@ -1,6 +1,10 @@
 package de.vsys.project.general.data;
 
 public class ForecastNecessaryLockdownDays {
+    /*
+    @author: David Rohrschneider
+    This class is the template for the data which we return to the user
+     */
     private int forecastNecessaryLockdownDays;
 
     public int getForecastNecessaryLockdownDays() {
--- VerteilteSystemeProjekt/src/main/java/de/vsys/project/general/data/IncidenceValueLastSevenDays.java
+++ VerteilteSystemeProjekt/src/main/java/de/vsys/project/general/data/IncidenceValueLastSevenDays.java
@@ -1,6 +1,10 @@
 package de.vsys.project.general.data;
 
 public class IncidenceValueLastSevenDays {
+    /*
+    @author: David Rohrschneider
+    This class is the template for the data which we return to the user
+     */
     private int incidenceValueLastSevenDays;
 
     public int getIncidenceValueLastSevenDays() {

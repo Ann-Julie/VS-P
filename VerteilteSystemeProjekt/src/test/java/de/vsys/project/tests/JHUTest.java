e7e4d22541703c85d843a820c4567d855340883e
mwitko <77248634+mwitko@users.noreply.github.com>
2021-02-12 10:02:58 +0100

    removed main method from RobertKochInstitutData

--- VerteilteSystemeProjekt/src/test/java/de/vsys/project/tests/JHUTest.java
+++ VerteilteSystemeProjekt/src/test/java/de/vsys/project/tests/JHUTest.java
@@ -7,14 +7,31 @@ import org.junit.jupiter.api.Test;
 public class JHUTest {
     /*
     @author: Mike Witkowski
+    This class tests the methods from the JohnHopkinsUniversityData class
+    For testing the methods, you have to calcute the values manually
      */
     JohnHopkinsUniversityData johnHopkinsUniversityData = new JohnHopkinsUniversityData();
-    @Test
-    public void test(){
-        Assertions.assertEquals(7,3+5);
-    }
+
+    //This method tests the method that checks the infections from the last twenty four hours
     @Test
     public void testInfectionsFromLastTwentyFourHours(){
-            Assertions.assertEquals(9928, johnHopkinsUniversityData.checkNewInfectionsFromLastTwentyFourHours());
+        Assertions.assertEquals(9928, johnHopkinsUniversityData.checkNewInfectionsFromLastTwentyFourHours());
     }
+    //This method tests the method that checks the total infections
+    @Test
+    public void testTotalInfections(){
+        Assertions.assertEquals(147493, johnHopkinsUniversityData.checkTotalInfections());
+
+    }
+    //This method tests the method that checks the average increase from the last n days, in this case we test for the last 7 days
+    @Test
+    public void testAverageIncreaseFromLastNDays(){
+        Assertions.assertEquals(-5300.857142857143,johnHopkinsUniversityData.checkAverageIncreaseFromLastNDays(7));
+    }
+    //This method tests the method that checks the increase from the last twenty four hours
+    @Test
+    public void testIncreaseFromLastTwentyFourHours(){
+        Assertions.assertEquals(-3357, johnHopkinsUniversityData.checkIncreaseFromLastTwentyFourHours());
+    }
+
 }

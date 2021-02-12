0c7ff85b120553e372973e3277987a2e47f8447c
mwitko <77248634+mwitko@users.noreply.github.com>
2021-02-12 16:29:53 +0100

    added new jar

--- VerteilteSystemeProjekt/src/main/java/de/vsys/project/soap/SoapServer.java
+++ VerteilteSystemeProjekt/src/main/java/de/vsys/project/soap/SoapServer.java
@@ -94,6 +94,6 @@ public class SoapServer {
 
 
     public static void main(String[] args) {
-        Endpoint.publish("http://localhost:4568/coronaData", new SoapServer());
+        Endpoint.publish("http://167.99.252.170:4568/coronaData", new SoapServer());
     }
 }

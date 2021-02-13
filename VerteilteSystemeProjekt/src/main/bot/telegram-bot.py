8ef44b453e204c69fc4233a37659c65fcd4b3f52
Ann-Julie Sinnaeve <annjulie.sinnaeve@yahoo.de>
2021-02-13 13:47:40 +0100

    telegram bot sends json data to app

--- VerteilteSystemeProjekt/src/main/bot/telegram-bot.py
+++ VerteilteSystemeProjekt/src/main/bot/telegram-bot.py
@@ -1,10 +1,14 @@
 """
- Um die Sachen im Browser anzuschauen, api.telegram.org/bot mit TokenNr /getUpdates aufrufen
- Wenn wir in der App bspw einen Text eingeben (natürlich im bot selbst) und die Seite im Browser neu laden, können
- wir uns den Text anzeigen lassen
+Um die Sachen im Browser anzuschauen, api.telegram.org/bot mit TokenNr /getUpdates aufrufen
+
+Wenn wir in der App bspw einen Text eingeben (natürlich im bot selbst) und die Seite im Browser neu laden, können
+wir uns den Text anzeigen lassen
+
+wir wollen mit dem bot diese rest api wrappen
+dafür schreiben wir für jeden Zugriffspunkt eine Methode, die den jeweiligen Wert ausgibt
+
 """
 from urllib import request
-
 import requests
 
 vSysBotToken = "1668868793:AAF5cLabcsGgRHK8ovTbTnMuJ7nMzQH-oGQ"
@@ -17,24 +21,21 @@ portNumber = 4567
 # getURL = "http://localhost:" + str(portNumber)
 getURL = "http://167.99.252.170:" + str(portNumber)
 
+"""
 def extract_result(dict):
     result_array = dict['result']
 
     #prüfen ob Array leer ist
+"""
 
 # method needed to send messages to the group chat
 def send_message(chatId, message):
     send_text = requests.post(sendURL + "?chat_id=" + str(chatId) + "&text=" + message)
-    data_raw = requests.get(send_text)
+    # this is needed so we won't get response [200] but the json
+    data_raw = requests.get(send_text).text
+    # print(data_raw)
     data = data_raw.json()
-    print(data)
-
-
-"""
-
-wir wollen mit dem bot diese rest api wrappen
-dafür schreiben wir für jeden Zugriffspunkt eine Methode, die den jeweiligen Wert ausgibt
-"""
+    return data_raw
 
 def get_all_data():
     endPoint = "/alldata"
@@ -69,15 +70,14 @@ def get_increase_last_twentyFour_hours():
     endPoint = "/increaselasttwentyfourhours"
     return requests.get(getURL + endPoint)
 
-r = send_message(vSysChatId, str(get_average_increase_last_n_days(5)))
-# send_message(vSysChatId, "was geht ab")
-print(r.text)
+#send_message(vSysChatId, str(get_average_increase_last_n_days(5)))
+# send_message(vSysChatId, "test")
+
+print(get_all_data().text)
+send_message(vSysChatId, get_all_data().text)
 
 """ 
-ab dem Zeitpunkt an dem es einen Server gibt, muss einfach nur die URL in getURL angepasst werden, feddich
-in den Chat mit send_Message einfügen, also nur überlegen wie Text von  A nach B kommt, jedesMal wenn Zugriffspunkt 
-aufgerufen wird
-todo morgen: Commands anschauen für Interaktion mit User in App
+todo morgen: Commands anschauen für Interaktion mit User in App und ganz viele if else ;) 
 """
 
 

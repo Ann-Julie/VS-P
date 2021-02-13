d5782408b9e8c3da23e97c52e59d7315a41055c3
jonahei <jheinzer@gmx.de>
2021-02-13 13:01:01 +0100

    fixed StateData (deleted String LAN_ew_GEN)

--- VerteilteSystemeProjekt/src/main/java/de/vsys/project/rki/StateData.java
+++ VerteilteSystemeProjekt/src/main/java/de/vsys/project/rki/StateData.java
@@ -7,11 +7,6 @@ public class StateData {
     This class is the template for the json data from the Robert Koch Institut
     */
     double cases7_bl_per_100k;
-    String LAN_ew_GEN;
-
-    public String getLAN_ew_GEN() {
-        return LAN_ew_GEN;
-    }
 
     public double getCases7_bl_per_100k() {
         return cases7_bl_per_100k;

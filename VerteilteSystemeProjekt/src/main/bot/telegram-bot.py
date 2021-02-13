2c200d33153ee0e51fec3074404235ec9fedeab0
Ann-Julie Sinnaeve <annjulie.sinnaeve@yahoo.de>
2021-02-13 12:16:06 +0100

    telegram bot response 200

--- VerteilteSystemeProjekt/src/main/bot/telegram-bot.py
+++ VerteilteSystemeProjekt/src/main/bot/telegram-bot.py
@@ -7,26 +7,31 @@ from urllib import request
 
 import requests
 
-# Link benötigt
 vSysBotToken = "1668868793:AAF5cLabcsGgRHK8ovTbTnMuJ7nMzQH-oGQ"
 vSysChatId = "-507253319"
 
-# Nachrichten versenden
-# neue URL benötigt
+# to send messages to the bot, the token and group chat id are needed
 sendURL = "http://api.telegram.org/bot" + vSysBotToken + "/sendMessage"
-# url für lokal host für die api später
 portNumber = 4567
 
-# get URL ist dann später für den Server, also mit lokal host austauschen
 # getURL = "http://localhost:" + str(portNumber)
 getURL = "http://167.99.252.170:" + str(portNumber)
 
+def extract_result(dict):
+    result_array = dict['result']
+
+    #prüfen ob Array leer ist
+
+# method needed to send messages to the group chat
 def send_message(chatId, message):
-    # die Ergebnisse aus den Variablen unten senden
-    requests.post(sendURL + "?chat_id=" + str(chatId) + "&text=" + message)
+    send_text = requests.post(sendURL + "?chat_id=" + str(chatId) + "&text=" + message)
+    data_raw = requests.get(send_text)
+    data = data_raw.json()
+    print(data)
 
 
 """
+
 wir wollen mit dem bot diese rest api wrappen
 dafür schreiben wir für jeden Zugriffspunkt eine Methode, die den jeweiligen Wert ausgibt
 """
@@ -35,20 +40,39 @@ def get_all_data():
     endPoint = "/alldata"
     return requests.get(getURL + endPoint)
 
+def get_total_infections():
+    endPoint = "/totalinfections"
+    return requests.get(getURL + endPoint)
+
+def get_new_infections_from_last_twentyFour_hours():
+    endPoint = "/newinfectionsfromlasttwentyfourhours"
+    return requests.get(getURL + endPoint)
+
+def get_target_total_infection():
+    endPoint = "/targettotalinfection"
+    return requests.get(getURL + endPoint)
+
+def get_forecast_necessary_lockdown_days():
+    endPoint = "/forecastnecessarylockdowndays"
+    return requests.get(getURL + endPoint)
+
+def get_incidence_value_last_seven_days():
+    endPoint = "/incidencevaluelastsevendays"
+    return requests.get(getURL + endPoint)
+
+
 def get_average_increase_last_n_days(days):
-    # über commands soll eine Abfrage erscheinen, nach Tagen
-    #
     endPoint = "/averageincreaselastndays/"
     return requests.get(getURL + endPoint + str(days))
 
+def get_increase_last_twentyFour_hours():
+    endPoint = "/increaselasttwentyfourhours"
+    return requests.get(getURL + endPoint)
 
-send_message(vSysChatId, str(get_average_increase_last_n_days(5)))
-# so bekommen wir nur das request Objekt und müssen jetzt noch die
-# Daten daraus herausholen
-r = get_average_increase_last_n_days(5)
-print(r)
+r = send_message(vSysChatId, str(get_average_increase_last_n_days(5)))
+# send_message(vSysChatId, "was geht ab")
 print(r.text)
-get_average_increase_last_n_days(5)
+
 """ 
 ab dem Zeitpunkt an dem es einen Server gibt, muss einfach nur die URL in getURL angepasst werden, feddich
 in den Chat mit send_Message einfügen, also nur überlegen wie Text von  A nach B kommt, jedesMal wenn Zugriffspunkt 

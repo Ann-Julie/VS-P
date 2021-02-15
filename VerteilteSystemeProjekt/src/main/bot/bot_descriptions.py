93fbc1f964f1a731c32c164d73d683da179e743e
Ann-Julie Sinnaeve <annjulie.sinnaeve@yahoo.de>
2021-02-15 23:16:05 +0100

    telegram bot  changed start message

--- VerteilteSystemeProjekt/src/main/bot/bot_descriptions.py
+++ VerteilteSystemeProjekt/src/main/bot/bot_descriptions.py
@@ -4,29 +4,29 @@ all larger text messages from telegram VSys-CoV bot
 """
 
 startCommandText = (
-    """ 
-    Herzlich Willkommen. Ich bin der VSys-CoV Bot! 
-    Alle meine Funktionen finden Sie mit /help. 
-    """)
+""" 
+Herzlich Willkommen. Ich bin der VSys-CoV Bot! 
+Alle meine Funktionen finden Sie mit /help. 
+""")
 
 helpCommandText = (
-    """ 
-    Auswahl: 
-    Alle Informationen:
-    /alldata 
-    Aktuelle Gesamtinfektion in Deutschland:
-    /totalinfec
-    Zahl Neuinfektionen letzte 24 Stunden:
-    /newinfeclast24h
-    Zielinzidenzwert der Gesamt Infektionen:
-    /targettotalinfec
-    Annahme Tage in Lockdown:
-    /forecastlockdays
-    Inzidenzwert der  letzten 7 Tage:
-    /incvallast7days 
-    Durchschnittlicher Anstieg 
-    der letzten n Tage:
-    /avgincrlastndays
-    Anstieg der letzten 24 Stunden:
-    /incrlast24h
-    """)
+""" 
+Auswahl: 
+Alle Informationen:
+/alldata 
+Aktuelle Gesamtinfektion in Deutschland:
+/totalinfec
+Zahl Neuinfektionen letzte 24 Stunden:
+/newinfeclast24h
+Zielinzidenzwert der Gesamt Infektionen:
+/targettotalinfec
+Annahme Tage in Lockdown:
+/forecastlockdays
+Inzidenzwert der  letzten 7 Tage:
+/incvallast7days 
+Durchschnittlicher Anstieg 
+der letzten n Tage:
+/avgincrlastndays
+Anstieg der letzten 24 Stunden:
+/incrlast24h
+""")
